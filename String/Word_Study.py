6276bd9fece71fc8f70e9ecf60d46579a117257e
GYU-BO David SHIM <59437638+Gyubster@users.noreply.github.com>
2020-08-17 20:50:48 +0900

    Update Word_Study.py

--- String/Word_Study.py
+++ String/Word_Study.py
@@ -3,6 +3,12 @@ List = []
 
 for i in set(Str_Input):
     List.append(Str_Input.count(i))
+
 Max_Count = [i for i, x in enumerate(List) if x == max(List)]
 
-# 해야됨
+if len(Max_Count)>1:
+    print("?")
+else:
+    print(list(set(Str_Input))[List.index(max(List))])
+
+# emurate를 이용하여 최대값이 있는 위치를 알게함.
