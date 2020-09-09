e0d8f22566bb3c4a59bf0d0175838ab248492b0a
GYU-BO David SHIM <59437638+Gyubster@users.noreply.github.com>
2020-09-09 20:24:21 +0900

    Update Snail.py

--- Math1/Snail.py
+++ Math1/Snail.py
@@ -1,3 +1,21 @@
+# ClimbUp, ClimbDown, Height = map(int, input().split())
+#
+# Day = 1
+# TotalHeight = 0
+#
+# while True:
+#     if TotalHeight < Height:
+#         TotalHeight += ClimbUp
+#         if TotalHeight == Height:
+#             print(Day)
+#         else:
+#             TotalHeight -= ClimbDown
+#     Day += 1
+#
+# print(Day)
+
+# 시간 초과로 인해서 다시 작성
+
 ClimbUp, ClimbDown, Height = map(int, input().split())
 
 Day = (Height-ClimbDown)/(ClimbUp-ClimbDown)
